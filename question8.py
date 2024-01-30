cbdba56ed74e83f27065078b9321753e9139c72b
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 23:13:56 -0500

    Update question8.py

--- question8.py
+++ question8.py
@@ -1,18 +1,15 @@
 def question8_1():
-    """
-    answer= 'string'  or ['pt1', 'pt2']
-    """
-    answer = 'equally similar'
+    answer = ['x1', 'x2']
     return answer
 
 def question8_2():
-    answer = 'equally similar'
+    answer= 'equally similar'
     return answer
 
 def question8_3():
-    answer = 'equally similar'
+    answer= 'equally similar'
     return answer 
 
 def question8_4():
-    answer = ['x2','y3']
-    return answer
+    answer = ['x2', 'y3']
+    return answer 

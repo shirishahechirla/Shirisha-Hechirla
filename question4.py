4ee425d249570b6009a78b3f79117ff2a64f4e47
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 19:53:09 -0500

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,24 +1,31 @@
 def question4_1():
-    # answer = ['string', 'string']
+    answer = ['nominal', 'ordinal']
     return answer
 
 def question4_2():
+    answer = ['ratio', 'interval']
     return answer
 
 def question4_3():
+    answer = ['ratio', 'ordinal']
     return answer 
 
 def question4_4():
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_5():
+    answer = ['ratio', 'ratio']
     return answer
 
 def question4_6():
+    answer = ['ratio', 'ordinal']
     return answer
 
 def question4_7():
+    answer = ['ratio', 'ratio']
     return answer
 
 def question4_8():
+    answer = ['ratio', 'ratio']
     return answer

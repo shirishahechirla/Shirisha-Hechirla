1126cd63ca368d60144c922827e4dee8366b8346
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 23:29:45 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -7,13 +7,11 @@ def question2_2():
     return answer
 
 def question2_3():
-    answer = ['Continuous', 'Quantitative', 'Ratio'] 
-    return answer
+    
+    return answer 
 
 def question2_4():
-    answer = ['Discrete', 'Qualitative', 'Ordinal'] 
     return answer
 
 def question2_5():
-    answer = ['Discrete', 'Qualitative', 'Nominal']
     return answer

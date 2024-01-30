fb3d7cde1a0f6a0437eb537db511cb923f2f6d44
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 23:11:07 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,12 +1,4 @@
 def question5_1():
-    """
-    answer = {
-            'bin1': [d1,d2,d3...]
-            'bin2': [e1,...],
-            'bin3': [f1,...]
-    }
-    where di, ei, fi are integers. 
-    """
     answer = {
             'bin1': [1,2,3,4,5],
             'bin2': [6,7,8],
@@ -24,8 +16,8 @@ def question5_2():
 
 def question5_3():
     answer = {
-            'bin1': [1,2,3,4],
-            'bin2': [5,6,7],
-            'bin3': [8,9]
+            'bin1': [1,5,6,7,9],   # Female
+            'bin2': [2,3,4],       # Male
+            'bin3': [8]            # Male
     }
     return answer

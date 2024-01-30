b3b29631b896079a7fdf52c4cd569f52162a8aa6
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 19:56:42 -0500

    Update question9.py

--- question9.py
+++ question9.py
@@ -2,17 +2,21 @@ def question9_1():
     """
     answer = string'
     """
+    answer = 'SMC'
     return answer
 
 def question9_2():
+    answer = 'Jaccard'
     return answer
 
 def question9_3():
+    answer = 'correlation coefficient'
     return answer 
 
 def question9_4():
+    answer = 'Cosine Similarity'
     return answer 
 
 def question9_5():
-    return answer 
-
+    answer = 'Cosine Similarity'
+    return answer

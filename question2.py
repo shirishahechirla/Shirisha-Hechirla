9d540e9a4adedc5d74157a4ac064cd5c5b669adf
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-01-29 19:50:26 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,15 +1,19 @@
 def question2_1():
-    # answer = ['string', 'string', 'string']
+    answer = ['discrete', 'quantitative', 'ratio']
     return answer
 
 def question2_2():
+    answer = ['continuous', 'quantitative', 'ratio',]
     return answer
 
 def question2_3():
+    answer = ['continuous', 'quantitative', 'interval',]
     return answer 
 
 def question2_4():
+    answer = ['discrete', 'qualitative', 'ordinal',]
     return answer
 
 def question2_5():
+    answer = ['discrete', 'qualitative', 'nominal',]
     return answer
